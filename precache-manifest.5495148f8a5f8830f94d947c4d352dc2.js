3bb0dd3b3625deb48b48c56d5351fa90d5f8ecb0
buddy <buddy@buddy.works>
2023-04-19 20:08:33 +0000

    23

---
+++ precache-manifest.5495148f8a5f8830f94d947c4d352dc2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59f1aa8a19b236a5304720d967e6375",
+    "url": "/SimpleReactForm/index.html"
+  },
+  {
+    "revision": "353524da5f4ecd1b166a",
+    "url": "/SimpleReactForm/static/js/2.01acbe88.chunk.js"
+  },
+  {
+    "revision": "8208c9f2db860958e7e0",
+    "url": "/SimpleReactForm/static/js/main.e61f0784.chunk.js"
+  },
+  {
+    "revision": "a1c04805d14025294d58",
+    "url": "/SimpleReactForm/static/js/runtime-main.15047c2e.js"
+  }
+]);
